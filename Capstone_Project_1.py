33753104900b721128d0885b28e1121419647419
SaikatSantra7192 <45893902+SaikatSantra7192@users.noreply.github.com>
2019-01-20 02:43:31 +0530

    Update Capstone_Project_1.py
    Update in the "if condition" representation inside win_check function

--- Capstone_Project_1.py
+++ Capstone_Project_1.py
@@ -30,7 +30,14 @@ def place_marker(board, marker, position):
 
 def win_check(board,mark):
     #if board[1]==mark and board[2]==mark and board[3]==mark or board[4]==mark and board[5]==mark and board[6]==mark or board[7]==mark and board[8]==mark and board[9]==mark or board[1]==mark and board[4]==mark and board[7]==mark or board[2]==mark and board[5]==mark and board[8]==mark or board[3]==mark and board[6]==mark and board[9]==mark or board[1]==mark and board[5]==mark and board[9]==mark or board[3]==mark and board[5]==mark and board[7]==mark:
-    if board[1]==board[2]==board[3]==mark or board[4]==board[5]==board[6]==mark or board[7]==board[8]==board[9]==mark or board[1]==board[4]==board[7]==mark or board[2]==board[5]==board[8]==mark or board[3]==board[6]==board[9]==mark or board[1]==board[5]==board[9]==mark or board[3]==board[5]==board[7]==mark:    
+    if board[1]==board[2]==board[3]==mark or \
+       board[4]==board[5]==board[6]==mark or \
+       board[7]==board[8]==board[9]==mark or \
+       board[1]==board[4]==board[7]==mark or \
+       board[2]==board[5]==board[8]==mark or \
+       board[3]==board[6]==board[9]==mark or \
+       board[1]==board[5]==board[9]==mark or \
+       board[3]==board[5]==board[7]==mark:    
         return True
     else:
         return False
